d0e6f6597933426a090c4b4201a3de845745183e
Cristian <martinezcristianjavier@gmail.com>
2026-01-26 17:04:50 +0000

    add ingest data script

---
+++ homework-1/ingest_data.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import click
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+@click.command()
+@click.option('--year', default=2021, help='Year of data to ingest')
+@click.option('--month', default=1, help='Month of data to ingest')
+@click.option('--pg_user', default='root', help='PostgreSQL user')
+@click.option('--pg_pass', default='root', help='PostgreSQL password')
+@click.option('--pg_host', default='localhost', help='PostgreSQL host')
+@click.option('--pg_port', default='5432', help='PostgreSQL port')
+@click.option('--pg_db', default='ny_taxi', help='PostgreSQL database')
+@click.option('--target_table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, help='Chunk size for data ingestion')
+def run(year, month, pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, chunksize):
+    """Ingest data into PostgreSQL database in chunks."""
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+
+    # Connect to postgresql running as a docker container
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+
+    # Ingest data in chunks
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize
+    )
+
+    first = True
+
+    for df_chunk in tqdm(df_iter):
+
+        if first:
+            # Create table schema (no data)
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists="replace"
+            )
+            first = False
+            print("Table created")
+
+        # Insert chunk
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists="append"
+        )
+
+        print("Inserted:", len(df_chunk))
+
+if __name__ == "__main__":
+    run()
+
+
+
+
