3a96817d20c9f2e6c0ecbe87ca6e3a068a85e3d3
Cristian Martinez <ithinkidumb@hotmail.com>
2026-02-24 19:24:42 +0100

    fix intermediate model

--- 04-analytics-engineering/dbt_project/models/intermediate/int_trips_unioned.sql
+++ 04-analytics-engineering/dbt_project/models/intermediate/int_trips_unioned.sql
@@ -1,6 +1,6 @@
 WITH green_tripdata AS (
     SELECT * FROM {{ ref('stg_green_tripdata') }}
 ),
-WITH yellow_tripdata AS (
+yellow_tripdata AS (
     SELECT * FROM {{ ref('stg_yellow_tripdata') }}
 ),
