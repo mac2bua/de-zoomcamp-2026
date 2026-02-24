4687fa7645ff35caff2585b71639ede02376b1b7
Cristian Martinez <ithinkidumb@hotmail.com>
2026-02-24 17:54:06 +0000

    green and yellow staging models working

--- 04-analytics-engineering/dbt_project/models/staging/stg_green_tripdata.sql
+++ 04-analytics-engineering/dbt_project/models/staging/stg_green_tripdata.sql
@@ -3,12 +3,12 @@ SELECT
     CAST(ratecodeid AS INT) as rate_code_id,
     CAST(pulocationid AS INT) as pickup_location_id,
     CAST(dolocationid AS INT) as dropoff_location_id,
-    -- CAST(lpep_pickup_datetime AS timestamp) as pickup_datetime,
-    -- CAST(lpep_dropoff_datetime AS timestamp) as dropoff_datetime,
+    CAST(lpep_pickup_datetime AS timestamp) as pickup_datetime,
+    CAST(lpep_dropoff_datetime AS timestamp) as dropoff_datetime,
     store_and_fwd_flag,
     CAST(passenger_count AS INT) as passenger_count,
     CAST(trip_distance AS numeric) as trip_distance,
-    -- CAST(trip_type AS INT) as trip_type,
+    CAST(trip_type AS INT) as trip_type,
     CAST(fare_amount AS numeric) as fare_amount,
     CAST(extra AS numeric) as extra,
     CAST(mta_tax AS numeric) as mta_tax,
@@ -18,3 +18,4 @@ SELECT
     CAST(total_amount AS numeric) as total_amount,
     CAST(payment_type AS numeric) as payment_type
 FROM {{ source('raw_data', 'green_tripdata') }}
+WHERE vendorid IS NOT NULL
