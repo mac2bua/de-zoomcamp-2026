dcba4615fc7e553d6c98dd874b76bf8b71122c6b
Cristian <martinezcristianjavier@gmail.com>
2026-04-01 07:38:38 +0000

    flink job is working now - hm7 q4 solution added

--- 07-streaming/homework-7/producer.py
+++ 07-streaming/homework-7/producer.py
@@ -2,31 +2,27 @@ import json
 import pandas as pd
 from kafka import KafkaProducer
 from time import time
+from models import ride_serializer, ride_from_row
 
 # Read parquet file
 df = pd.read_parquet('data/green_tripdata_2025-10.parquet')
 
 # Keep only required columns
 columns = [
-    'lpep_pickup_datetime',
-    'lpep_dropoff_datetime',
     'PULocationID',
     'DOLocationID',
-    'passenger_count',
     'trip_distance',
-    'tip_amount',
-    'total_amount'
+    'total_amount',
+    'lpep_pickup_datetime'
 ]
 df = df[columns]
 
 # Convert datetime columns to strings
 df['lpep_pickup_datetime'] = df['lpep_pickup_datetime'].astype(str)
-df['lpep_dropoff_datetime'] = df['lpep_dropoff_datetime'].astype(str)
-
 # Create Kafka producer
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9092'],
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    value_serializer=ride_serializer
 )
 
 # Send all rows
@@ -34,7 +30,8 @@ t0 = time()
 
 for _, row in df.iterrows():
     message = row.to_dict()
-    producer.send('green-trips', value=message)
+    ride = ride_from_row(message)
+    producer.send('green-trips', value=ride)
     producer.flush()  # Ensure message is sent
 
 t1 = time()
